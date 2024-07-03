c003491b1466bda7e94b7c01ac754e76d332b88c
llitalk <litalkaminsky@gmail.com>
2024-07-03 10:53:40 +0300

    changes

---
+++ src/app/trade/charts-enums.enum.ts
@@ -0,0 +1,25 @@
+export enum Producttypes {
+    Index = 0,
+    NotIndex = 1
+}
+
+export enum PeriodType {
+    Daily = 0
+}
+
+export enum ChartType {
+    Line = 0,
+    Area = 1,
+    Bhlc = 2,
+    Candles = 3
+}
+
+export enum ChartDisplay {
+    Rate = 0,
+    Precentage = 0
+}
+
+export enum ChartLinkage {
+    Nominiak = 0,
+    InDollar = 1
+}
